b22bca20ad742c8a8aef86e9c207ee7e55ed7a86
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-19 21:32:59 -0700

    Update Char-code-calculation.js

--- Char-code-calculation.js
+++ Char-code-calculation.js
@@ -18,3 +18,10 @@ Then return the difference between the sum of the digits in total1 and total2:
 
 
 // Solution
+
+function calc(x){
+  let sum = n => [...n].reduce((a,b) => +a + +b);
+  let total1 = x.replace(/./g, x => x.charCodeAt(0));
+  let total2 = total1.replace(/7/g,'1');
+  return sum(total1) - sum(total2);
+}
