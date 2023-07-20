dda17103efa2ba4cd2a113026eeb3a5332f0003b
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-19 21:33:29 -0700

    Update Char-code-calculation.js

--- Char-code-calculation.js
+++ Char-code-calculation.js
@@ -27,3 +27,14 @@ function calc(x){
 }
 
 // or
+
+function calc(x){
+  return x
+    .split('')
+    .map(c => c.charCodeAt(0))
+    .join('')
+    .split('')
+    .map(Number)
+    .filter(x => x === 7)
+    .length * 6
+}
